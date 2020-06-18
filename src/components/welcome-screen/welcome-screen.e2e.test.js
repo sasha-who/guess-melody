b7113eb672593a86c98d67b3f19afdb5f436aab5
sasha-who <warmsasha@gmail.com>
2020-06-18 11:57:57 +0300

    Добавляет e2e тест для компонента  WelcomeScreen

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import WelcomeScreen from "./welcome-screen.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const Settings = {
+  ERRORS_COUNT: 3
+};
+
+it(`WelcomeButton should be pressed`, () => {
+  const onWelcomeButtonClick = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        errorsCount={Settings.ERRORS_COUNT}
+        onWelcomeButtonClick={onWelcomeButtonClick}
+      />
+  );
+
+  const welcomeButton = welcomeScreen.find(`.welcome__button`);
+
+  welcomeButton.props().onClick();
+  expect(onWelcomeButtonClick.mock.calls.length).toBe(1);
+});
